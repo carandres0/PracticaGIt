199537506d1b7726f99aac406dc6743b6fc938ee
carandres0 <carandresgarcia@hotmail.com>
2025-08-28 13:06:53 -0500

    carlosgarcia

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("varios comandos");
+
+        //actividad
+        // rama con nombre
+        // la rama debajo de los comandos agregar nombre completo, lugar de viviendo y si tengo mascota tipo y nombre
+        System.out.println("carlos andres garcia ");
+        System.out.println("vivo en chia");
+        System.out.println("tengo una perrita y se llama ruby");
     }
 }
